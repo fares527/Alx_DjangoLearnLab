24d26459bc57e3025c5ed4668e105a13278834a4
fares ayman <149837282+fares527@users.noreply.github.com>
2025-02-20 13:41:03 +0200

    d

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/login.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
