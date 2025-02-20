77e16e08253524ae799787c5413480712e62b7f7
fares ayman <149837282+fares527@users.noreply.github.com>
2025-02-20 13:26:41 +0200

    s

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.views.generic import DetailView
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, aauthenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def list_books(request):
@@ -14,3 +17,52 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library' 
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = aauthenticate(username=username, password=password)
+        if user is not None:
+             login(request, user)
+             return redirect('profile') # Redirect to user's profile or any other page
+        else:
+                form.add_error(None, "Invalid username or password")
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+     logout(request)
+     return redirect('login')
+
+def user_register(request):
+    if request.method == 'POST':
+          form = UserCreationForm(request.POST)
+          if form.is_valid() :
+               user = form.save()
+               login(request, user)
+               return redirect('profile')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})            
+
+
+
+@login_required
+def profile(request):
+    """Example protected view, requires login."""
+    return render(request, 'profile.html', {'user': request.user})
+
+
+
+     
+
+
+
+
+
