1dba1fd480084614f35901c3f142bdb72652cf64
fares ayman <149837282+fares527@users.noreply.github.com>
2025-02-24 16:36:10 +0200

    a

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookshelf'
     'myapp'
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,4 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'myapp.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
