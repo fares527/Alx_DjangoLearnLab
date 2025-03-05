4894cd919d899e2991ccafb5a36240bf1ec96470
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-05 15:00:52 +0200

    s

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializers
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    permission_classes = [permissions.IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
