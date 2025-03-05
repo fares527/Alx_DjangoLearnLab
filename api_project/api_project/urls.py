3bc2b2c11f20e777203d32c64954535ff597b415
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-05 15:01:30 +0200

    s

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
