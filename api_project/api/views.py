e444e83c2aac344763adf3c29c3dbec14c87166c
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-05 14:50:39 +0200

    s

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializers
-from rest_framework import generics
+from rest_framework import generics, viewsets
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
