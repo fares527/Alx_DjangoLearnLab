71ff9345a1c72d4df2550b005a761441486e741d
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-14 14:48:56 +0200

    s

---
+++ advanced_api_project/advanced_api_project/api/views.py
@@ -0,0 +1,33 @@
+
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+
+# Create your views here.
+class BookListView(generics.ListAPIView):
+    # to view all books
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset= Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
