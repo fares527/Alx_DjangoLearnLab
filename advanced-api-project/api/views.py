e8d9daf7eab4f9063587fd59f29415c40003cb04
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-16 14:25:53 +0200

    a

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # Create your views here.
