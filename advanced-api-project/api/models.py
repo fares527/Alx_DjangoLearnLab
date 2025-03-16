31ecc670eb34e7d57f84655bfea8879c86c9f234
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-16 14:21:25 +0200

    a

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -12,5 +12,5 @@ class Book(models.Model):
     safcnja
     """
     title = models.CharField(max_length=200)
-    publication_date = models.IntegerField()
+    publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
