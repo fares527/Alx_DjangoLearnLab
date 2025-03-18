e260cdbcd03868b1e97afef786fc8655bc2cba6f
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-18 15:11:39 +0200

    s

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login_view/', views.login_view, name='login_view'),
-    path('logout_view/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
     path('profile/', views.profile, name='profile'),
 
 
