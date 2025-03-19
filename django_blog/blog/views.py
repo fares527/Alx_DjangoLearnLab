8cb5a089666623657f3614e1556c1ca3beeb31f5
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-19 14:49:16 +0200

    s

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
