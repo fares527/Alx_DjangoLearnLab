c03fc190e2711cfd3d48ddbeffe9730cd97edc83
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-19 14:46:56 +0200

    s

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
     path('profile/', views.profile, name='profile'),
-    path('post/list', views.PostListView.as_view, name='post_list'),
-    path('post/detail', views.PostDetailView.as_view, name='post_detail'),
-    path('post/create', views.PostCreateView.as_view, name='post_create'),
-    path('post/update', views.PostUpdateView.as_view, name='post_update'),
-    path('post/delete', views.PostDeleteView.as_view, name='post_delete'),
+    path('', views.PostListView.as_view, name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view, name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view, name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view, name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view, name='post_delete'),
 
 
 
