6d0d9c9e3553b4f0b08897df7c4ce45149c30195
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-20 13:53:18 +0200

    s

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view, name='post_new'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view, name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view, name='post_delete'),
-    path('post/<int:pk>/comments/new', views.post_detail.as_view, name=''post_detail),
     path('comment/<int:pk>/update/', views.CommentUpdataView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
     path('comment/<int:pk>/create/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('post/<int:pk>/comments/new', views.post_detail.as_view, name='post_detail'),
 
 
 
