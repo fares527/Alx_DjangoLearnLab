3908f1d0a72904bd1c0ea7035a0b15ce06307ceb
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-22 14:09:49 +0200

    s

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    tags = TaggableManager()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
 
 class Comment(models.Model):
