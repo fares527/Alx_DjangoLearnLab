6f8362062946c25b20add20d57a60f5cc4c81cec
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-24 14:44:50 +0200

    s

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model,authenticate
+from rest_framework.authtoken.models import Token
 
-
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     model = User
