20689376706b4c04c408b7d93d9e2216d8e9d120
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-27 14:23:35 +0200

    a

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .serializers import LoginSerializer, UserSerializer
-from .views import UserCreate, UserProfile, LoginUser
+from .views import UserCreate, UserProfile, LoginUser, follow_user, unfollow_user
 
 urlpatterns = [
     path('register/',UserCreate.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
-    path('profile/', UserProfile.as_view(), name='profile')
+    path('profile/', UserProfile.as_view(), name='profile'),
+    path('follow/<int:user_id>/', follow_user.as_view()),
+    path('unfollow/<int:user_id>/', unfollow_user.as_view),
+    #unfollow/<int:user_id>/", "follow/<int:user_id>"]
 ]
