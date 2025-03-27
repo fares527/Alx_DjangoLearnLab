25dfa31729c4da03e3601049391bec7206c1a041
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-27 14:17:51 +0200

    s

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -26,7 +26,7 @@ class UserProfile():
 @api_view('POST')
 @permission_classes([permissions.IsAuthenticated])
 def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    user_to_follow = CustomUser.objects.all()
     if request.user != user_to_follow:
         request.user.following.add(user_to_follow)
         return Response({'message': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
@@ -36,7 +36,7 @@ def follow_user(request, user_id):
 @api_view('POST')
 @permission_classes([permissions.IsAuthenticated])
 def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    user_to_unfollow = CustomUser.objects.all()
     request.user.following.remove(user_to_unfollow)
     return  Response({'message': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
 
