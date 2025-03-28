34a90269327e623cce3c9a74a4c17fb35eafd0aa
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-28 13:55:32 +0200

    a

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'postgres',
+          'USER': 'postgres',
+          'PASSWORD': 'postgre',
+          'HOST': 'your_database_host',
+          'PORT': '5432',
+      }
+  }
 
 
 # Password validation
