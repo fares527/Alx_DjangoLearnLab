97e8fbc7c2cee619ea01c37d66a4c72e81989157
fares ayman <149837282+fares527@users.noreply.github.com>
2025-03-28 13:40:10 +0200

    a

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -27,5 +27,5 @@ class Comment(models.Model):
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
